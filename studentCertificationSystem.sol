d84d0fdb4aad51546cd06d4abcacfdea0d02e2ef
Pankhuri Varshney <pankhu.var@gmail.com>
2023-10-20 15:54:38 +0530

    Basic Structure of Student Certification System

---
+++ studentCertificationSystem.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract studentCertificationSystem {
+
+    function addCertificate(address studentAddress, string memory studentName, string memory certificateName) public {
+    }
+
+    function verifyCertificate(address studentAddress, uint certIndex) public {
+    }
+
+    function displayAll(address studentAddress) public view returns(Certificate[] memory){
+    }
