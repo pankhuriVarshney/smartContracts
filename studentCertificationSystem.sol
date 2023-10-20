140ae75d9b2fba9391b526aa3bc7f682e6a2f27c
Pankhuri Varshney <pankhu.var@gmail.com>
2023-10-20 15:58:10 +0530

    Add struct for Student information and Certificate information

--- studentCertificationSystem.sol
+++ studentCertificationSystem.sol
@@ -3,6 +3,22 @@ pragma solidity ^0.8.0;
 
 contract studentCertificationSystem {
 
+    struct Student {
+        string name;
+        address studentAddress;
+        uint certCount;
+    }
+
+    struct Certificate {
+        string certName;
+        bool verified;
+    }
+
+    mapping(address => Student) public students;
+    mapping(address => Certificate[]) public certificates;
+    address public admin;
+    uint count=0;
+    
     function addCertificate(address studentAddress, string memory studentName, string memory certificateName) public {
     }
 
