4784baa3eb130b47daff48c88bbfcb027dc9da38
Pankhuri Varshney <pankhu.var@gmail.com>
2023-10-20 16:08:11 +0530

    Add functions to add a candidate, set the votes and display the votes

--- votingSystem.sol
+++ votingSystem.sol
@@ -20,4 +20,24 @@ contract _votingSystem
         admin=msg.sender;
     }
     
+    function addCandidate(string memory candidateName, address candidateAdd) public  
+    {
+        require(admin==msg.sender,"NOT ADMIN. PERMISSION DENIED");
+        candidates.push(Candidate({
+                name: candidateName, candidateAddress: candidateAdd,
+                voteCount: 0}));
+    }    
+
+    function setVote(uint candidateIndex) public {
+        require(!voters[msg.sender]||admin!=msg.sender, "You have already voted. NOTE: ADMIN DOES NOT HAVE VOTING PERMISSIONS");
+        require(candidateIndex < candidates.length&&candidateIndex>=0, "Invalid candidate index. Refer to candidate list");
+
+        candidates[candidateIndex].voteCount++;
+        voters[msg.sender] = true;
+    }
+
+    function getVotes() public view returns (Candidate[] memory){
+        return candidates;
+    }
+ 
 }
