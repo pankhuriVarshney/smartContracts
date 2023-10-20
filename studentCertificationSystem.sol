42295b66faf243219d33f3411a2d3d9addea51cf
Pankhuri Varshney <pankhu.var@gmail.com>
2023-10-20 16:00:54 +0530

    Add functions to add certificate into blockchain record, to verify the certificates (allowed only for the admin) and to display all certificates of a student

--- studentCertificationSystem.sol
+++ studentCertificationSystem.sol
@@ -18,12 +18,26 @@ contract studentCertificationSystem {
     mapping(address => Certificate[]) public certificates;
     address public admin;
     uint count=0;
-    
+
     function addCertificate(address studentAddress, string memory studentName, string memory certificateName) public {
+        count++;
+        students[studentAddress] = Student({
+            name: studentName,
+            studentAddress: studentAddress,
+            certCount: count});
+        Certificate[] storage studentCertificates = certificates[studentAddress];
+        studentCertificates.push(Certificate({
+            certName: certificateName,
+            verified: false
+        }));
     }
 
     function verifyCertificate(address studentAddress, uint certIndex) public {
+        require(msg.sender == admin, "NOT ADMIN, PERMISSION DENIED");
+        require(certIndex < certificates[studentAddress].length, "CERTIFICATE ID DOES NOT EXIST");
+        certificates[studentAddress][certIndex].verified = true;
     }
 
     function displayAll(address studentAddress) public view returns(Certificate[] memory){
+        return certificates[studentAddress];
     }
